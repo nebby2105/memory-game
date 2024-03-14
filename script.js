c270a19e4480ea1af4d6c177ad1762c59f89cf86
nebby2105 <lopatkaoa@example.com>
2024-03-14 08:09:32 +0200

    deleted comments

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// const symbols = ['🍉', '🍎', '🍋', '🍇', '🍒', '🍓', '🥥', '🍍'];
 const images = ['img/spider1.jpg', 'img/spider2.jpg', 'img/spider3.jpg', 'img/spider4.jpg', 'img/spider5.jpg', 'img/spider6.jpg', 'img/spider7.jpg', 'img/spider8.jpg'];
 const doubledImages = [...images, ...images];
 const grid = document.querySelector('.grid');
@@ -113,74 +112,5 @@ function showModal() {
 }
 
 
-
-
-// const symbols = ['🍉', '🍎', '🍋', '🍇', '🍒', '🍓', '🥥', '🍍'];
-// const doubledSymbols = [...symbols, ...symbols];
-// const grid = document.querySelector('.grid');
-// let cardsChosen = [];
-// let cardsChosenId = [];
-// let cardsMatched = [];
-
-// class Card {
-//     constructor(symbol) {
-//         this.symbol = symbol;
-//         this.isFlipped = false;
-//         this.cardElement = this.createCardElement();
-//         this.frontSide = this.cardElement.querySelector('.front');
-//         this.backSide = this.cardElement.querySelector('.back')
-//         this.cardElement.addEventListener('click', this.flip.bind(this));
-//     }
-
-//     createCardElement() {
-//         const card = document.createElement('div');
-//         card.classList.add('card');
-        
-//         const frontSide = document.createElement('div');
-//         frontSide.classList.add('front');
-//         frontSide.textContent = 'Spider'; // Initial content
-//         this.frontSide = frontSide; // Save a reference for later use
-        
-//         const backSide = document.createElement('div');
-//         backSide.classList.add('back');
-//         backSide.textContent = this.symbol;
-//         this.backSide = backSide; // Save a reference for later use
-        
-//         card.appendChild(frontSide);
-//         card.appendChild(backSide);
-    
-//         return card;
-//     }
-
-//     flip() {
-//         if (!this.isFlipped && cardsChosen.length < 2) {
-//             this.cardElement.classList.add('flipped');
-//             this.isFlipped = true;
-//             // Update the content of the front side to the symbol
-//             this.frontSide.textContent = this.symbol;
-//             cardsChosen.push(this.symbol);
-//             cardsChosenId.push(this.cardElement);
-//             if (cardsChosen.length === 2) {
-//                 setTimeout(checkForMatch, 500);
-//             }
-//         } else if (this.isFlipped && cardsChosen.length === 2) {
-//             this.cardElement.classList.remove('flipped');
-//             this.isFlipped = false;
-//             // Revert the content of the front side to "Spider"
-//             this.frontSide.textContent = 'Spider';
-//             cardsChosen.pop();
-//             cardsChosenId.pop();
-//         }
-//     }
-// }
-
-// function generateCards() {
-//     for (let i = 0; i < doubledSymbols.length; i++) {
-//         const card = new Card(doubledSymbols[i]);
-//         grid.appendChild(card.cardElement);
-//     }
-// }
- 
-// generateCards();
 
 
